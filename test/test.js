1ab7225c84d99d1306de27fa9865a64fd57b428a
koyayashi1231 <kobayashi.for.develop@gmail.com>
2019-09-13 13:43:03 +0900

    fix: ローカルでの開発までスクリーンショット撮る用

--- test/test.js
+++
@@ -1,8 +0,0 @@
-const should = require('should');
-
-describe('getScore', () => {
-  it('sが０である', () => {
-	var s = 0
-	s.should.equal(0);
-  });
-});
---
+++ test/teststest.js
@@ -0,0 +1,8 @@
+const should = require('should');
+
+describe('テストのテスト', () => {
+  it('０は０である', () => {
+	var v = 0
+	v.should.equal(0);
+  });
+});
